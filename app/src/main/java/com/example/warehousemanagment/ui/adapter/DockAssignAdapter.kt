46a2c92df9caad71dc6d649216b6dbcc481d31a6
mohyeddin <m.imanih.h20@gmail.com>
2024-11-18 17:05:52 +0330

    fix bug

--- app/src/main/java/com/example/warehousemanagment/ui/adapter/DockAssignAdapter.kt
+++ app/src/main/java/com/example/warehousemanagment/ui/adapter/DockAssignAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.warehousemanagment.databinding.PatternDockAssignBinding
+import com.example.warehousemanagment.model.classes.getBuiltString
 import com.example.warehousemanagment.model.models.dock_assign.ShippingListOnDockRow
 
 class DockAssignAdapter(
@@ -29,7 +30,13 @@ class DockAssignAdapter(
         holder.binding.bolNumber.text = model.bOLNumber
         holder.binding.shippingNumber.text = model.shippingNumber
         holder.binding.driverFullName.text = model.firstDriverFullName
-        holder.binding.plaque.text = model.carPlaqueNumber
+        val plques = model.carPlaqueNumber.split('-')
+        if (plques.size==4){
+            holder.binding.plaque.text = getBuiltString(
+                plques[3], plques[2], plques[1]
+            )
+            holder.binding.plaqueYear.text = plques[0]
+        }
         holder.binding.type.text = model.carTypeTitle
 
         holder.binding.root.setOnClickListener {
